82eb15ec74367cf07880fa4cf5b0fce516667d64
Laurent Dietrich <dietrich.laurent@gmail.com>
2023-12-11 16:40:23 +0100

    day 11

---
+++ 11.py
@@ -0,0 +1,59 @@
+map = [ ] # recreating the map
+with open('11', 'r') as f:
+    for ligne in f.read().splitlines():
+        map.append(ligne)
+
+
+n, m = len(map), len(map[0])
+
+emptyrows, emptycols = [], []
+
+for i, ligne in enumerate(map):
+    if ligne == '.'*m:
+        emptyrows.append(i)
+
+for j in range(m):
+    if [map[i][j] for i in range(n)] == ['.']*n:
+        emptycols.append(j)
+
+stars = [ ]
+for i in range(n):
+    for j in range(m):
+        if map[i][j] == '#': stars.append( (i,j) )
+
+
+def dist( star1, star2 ):
+    i, j = star1
+    k, l = star2
+    d = abs(i-k) + abs(j-l)
+
+    for row in emptyrows:
+        if i < row < k or k < row < i: d += gap
+    for col in emptycols:
+        if j < col < l or l < col < j: d += gap
+
+    return d
+
+
+gap = 1
+
+sumdist = 0
+for (i,j) in stars:
+    for (k,l) in stars:
+        if (i,j) < (k,l):
+            d = dist( (i,j), (k,l)   )
+            sumdist += d
+
+print('Part 1 :', sumdist)
+
+## Part 2
+gap = 1000000 - 1
+
+sumdist = 0
+for (i,j) in stars:
+    for (k,l) in stars:
+        if (i,j) < (k,l):
+            d = dist( (i,j), (k,l)   )
+            sumdist += d
+
+print('Part 2 :', sumdist)
