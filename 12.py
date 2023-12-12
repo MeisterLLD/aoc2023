6a83899c0e43fd3b2cc12fed8f206db7836f5118
Laurent Dietrich <dietrich.laurent@gmail.com>
2023-12-12 23:32:40 +0100

    cleanup

--- 12.py
+++ 12.py
@@ -13,17 +13,17 @@ def nbconf( record, groups ):
     if record[0] == '.': # si on démarre par un .
         return nbconf( record[1:], groups)
 
-    nb1, nb2 = 0, 0
+    nb = 0
     if record[0] == '?': # ... par un ?
-        nb1 = nbconf( record[1:], groups) # possibilités en mettant un . à la place du ?
+        nb += nbconf( record[1:], groups) # possibilités en mettant un . à la place du ?
 
     # Possibilités avec le premier groupe de # au début
     # On veut tout le début sans . et on veut qu'à l'indice taillebloc il n'y ait pas un #
-    # Alors on peut placer tout le 1er bloc au début (obligatoire si on commence on veut commencer par un #)
+    # Alors on peut placer tout le 1er bloc au début (obligatoire si on commence ou veut commencer par un #)
     if '.' not in record[:groups[0]] and (len(record) <= groups[0] or len(record) > groups[0] and record[groups[0]] != '#'):
-            nb2 = nbconf( record[groups[0]+1:], groups[1:]   )
+            nb += nbconf( record[groups[0]+1:], groups[1:]   )
 
-    return nb1 + nb2
+    return nb
 
 
 somme = 0
