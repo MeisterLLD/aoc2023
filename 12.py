fb0a1100ebb7dac8926b75b46be9c51c994b02fe
Laurent Dietrich <dietrich.laurent@gmail.com>
2023-12-12 18:49:01 +0100

    explanations

--- 12.py
+++ 12.py
@@ -18,7 +18,7 @@ def nbconf( record, groups ):
         nb1 = nbconf( record[1:], groups) # possibilités en mettant un . à la place du ?
 
     # Possibilités avec le premier groupe de # au début
-    # On veut tout un début sans . et on veut qu'à l'indice taillebloc + 1 il y ait pas un #
+    # On veut tout le début sans . et on veut qu'à l'indice taillebloc + 1 il y ait pas un #
     # Alors on peut placer tout le 1er bloc au début
     if '.' not in record[:groups[0]] and (len(record) <= groups[0] or len(record) > groups[0] and record[groups[0]] != '#'):
             nb2 = nbconf( record[groups[0]+1:], groups[1:]   )
@@ -40,7 +40,7 @@ somme = 0
 with open('12', 'r') as f:
     for line in f.read().splitlines():
         record, groups = line.split(' ')
-        record = (5*(record + '?'))[:-1]
+        record = (5*(record + '?'))[:-1] # attention, pas de ? à la fin
         groups = 5*[int(x) for x in groups.split(',')]
         somme += nbconf( tuple(record) , tuple(groups) )
 
