92741582ef6d19ab0144a0d2770dfbfdd07ca1d1
Laurent Dietrich <dietrich.laurent@gmail.com>
2023-12-12 18:48:04 +0100

    explanations

--- 12.py
+++ 12.py
@@ -2,13 +2,14 @@ from functools import cache
 
 @cache
 def nbconf( record, groups ):
+
     # Cas de base
     if len(groups) == 0: # plus de # à placer
         return 1 if '#' not in record else 0
-
     if sum(groups) > len(record): # plus assez de place pour les #
         return 0
 
+    # Récursion
     if record[0] == '.': # si on démarre par un .
         return nbconf( record[1:], groups)
 
@@ -16,7 +17,9 @@ def nbconf( record, groups ):
     if record[0] == '?': # ... par un ?
         nb1 = nbconf( record[1:], groups) # possibilités en mettant un . à la place du ?
 
-    # possibilités avec le premier groupe de # au début
+    # Possibilités avec le premier groupe de # au début
+    # On veut tout un début sans . et on veut qu'à l'indice taillebloc + 1 il y ait pas un #
+    # Alors on peut placer tout le 1er bloc au début
     if '.' not in record[:groups[0]] and (len(record) <= groups[0] or len(record) > groups[0] and record[groups[0]] != '#'):
             nb2 = nbconf( record[groups[0]+1:], groups[1:]   )
 
@@ -28,7 +31,7 @@ with open('12', 'r') as f:
     for line in f.read().splitlines():
         record, groups = line.split(' ')
         groups = [int(x) for x in groups.split(',')]
-        somme += nbconf( tuple(record) , tuple(groups) )
+        somme += nbconf( tuple(record) , tuple(groups) ) # tuples pour pouvoir hasher...
 
 print('Part 1 :', somme)
 
