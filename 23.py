d55dd893de718ca663f419db960eafbfe8daae43
Laurent Dietrich <dietrich.laurent@gmail.com>
2023-12-24 22:30:26 +0100

    faster

--- 23.py
+++ 23.py
@@ -24,10 +24,10 @@ def DFS(start, end, init_dist, vus):
         yield init_dist
 
     for v in voisins(start):
-        vus2 = vus.copy() if len(voisins(start)) > 2 else vus
-        if v not in vus2:
-            vus2.add(v)
-            yield from DFS(v, end, init_dist + 1, vus2)
+        if v not in vus:
+            vus.add(v)
+            yield from DFS(v, end, init_dist + 1, vus)
+            vus.remove(v)
 
 deb = (0,1)
 fin = (n-1, m-2)
@@ -35,6 +35,7 @@ fin = (n-1, m-2)
 print('Part 1 :',max(DFS(deb,fin,0,{deb})))
 
 ## Part 2
+from time import time
 def voisins2( pos ):
     i, j = pos
     voisinspot = [(i-1,j), (i+1,j), (i,j-1), (i,j+1) ]
@@ -43,7 +44,9 @@ def voisins2( pos ):
 
 # Contracted graph
 bifurcations = [deb] + [(i,j) for i in range(n) for j in range(m) if len(voisins2((i,j))) > 2  ] + [fin]
-G = {bif: [ ] for bif in bifurcations}
+#G = {bif: [ ] for bif in bifurcations}
+from collections import defaultdict
+G = defaultdict(list)
 for b in bifurcations:
     for v in voisins2(b):
         previous, cur = b, v
@@ -60,6 +63,8 @@ def DFS2(start, end, init_dist, vus):
 
     for v, d in G[start]:
         if v not in vus:
-            yield from DFS2(v, end, init_dist + d, vus | {v})
+            vus.add(v)
+            yield from DFS2(v, end, init_dist + d, vus)
+            vus.remove(v)
 
 print('Part 2 :',max(DFS2(deb,fin,0,{deb})))
